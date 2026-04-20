461a16ce134c816b339d819a773d4891ff6b33af
Pranjal <pranjalmanikpuri@gmail.com>
2026-04-20 09:05:53 +0530

    Deploy 2

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World! 1234')
+    res.render('index', { title: 'Read Online - Athena' })
+})
+
+app.get('/browse', (req, res) => {
+    res.render('browse', {
+        title: 'Solo Leveling',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quod beatae cumque dicta dolore nisi!',
+        genre: 'Action' 
+    })
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
+
+
+
